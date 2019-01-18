a505af48b7033ca4b5015b8857e7efd4382acb12
Kevin Kim <kevinkim75@gmail.com>
2019-01-18 18:21:34 -0500

    Completed the app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express     = require('express');
+var helmet      = require('helmet');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
@@ -10,6 +11,8 @@ var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -22,6 +25,83 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.route('/')
   .get(function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.route('/api/convert')
+  .get(function (req, res) {
+    var input = decodeURIComponent(req.query.input || '');
+    var char = input.match(/[a-zA-Z]/);
+    var index = input.indexOf(char);
+    var result = {};
+    var numStr = index > -1 ? input.slice(0, index) : input;
+    var numArr = numStr.split('/');
+    if (numArr.length === 2) {
+      result.initNum = +numArr[0] / +numArr[1];
+    } else {
+      result.initNum = +numStr;
+    }
+
+    if (index === 0) {
+      result.initNum = 1;
+    }
+
+    result.initUnit = index > -1 ? input.slice(index).toLowerCase() : 'invalid unit';
+    switch (result.initUnit) {
+      case 'gal':
+        result.returnNum = result.initNum * 3.78541;
+        result.returnUnit = 'L';
+        break;
+      case 'l':
+        result.returnNum = result.initNum / 3.78541;
+        result.returnUnit = 'gal';
+        break;
+      case 'lbs':
+        result.returnNum = result.initNum * 0.453595;
+        result.returnUnit = 'kg';
+        break;
+      case 'kg':
+        result.returnNum = result.initNum / 0.453595;
+        result.returnUnit = 'lbs';
+        break;
+      case 'mi':
+        result.returnNum = result.initNum * 1.60934;
+        result.returnUnit = 'km';
+        break;
+      case 'km':
+        result.returnNum = result.initNum / 1.69934;
+        result.returnUnit = 'mi';
+        break;
+      default:
+        result.returnNum = result.initNum;
+        result.initUnit = 'invalid unit';
+        result.returnUnit = 'invalid unit';
+        break;
+    }
+
+    if (isNaN(result.initNum)) {
+      result.initNum = 'invalid number';
+    } else {
+      result.initNum = result.initNum.toFixed(5);
+    }
+
+    if (isNaN(result.returnNum)) {
+      result.returnNum = 'invalid number';
+    } else {
+      result.returnNum = result.returnNum.toFixed(5);
+    }
+
+    var unitMap = {
+      gal: 'gallons',
+      L: 'liters',
+      lbs: 'pounds',
+      kg: 'kilograms',
+      mi: 'miles',
+      km: 'kilometers',
+      'invalid unit': 'invalid unit',
+    };
+
+    result.string = result.initNum + ' ' + unitMap[result.initUnit] + ' converts to ' + result.returnNum + ' ' + unitMap[result.returnUnit];
+    res.json(result);
   });
 
 //For FCC testing purposes
